11077f2f85969d78c452e33dbe95bfdfe594f821
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-02-10 11:02:08 -0600

    Implement simple command line arguement parsing

---
+++ hw1/hw1_v2.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
+
+using namespace std;
+
+int setLines(int lines) {
+    if(lines <= 0)  {
+        return 0;
+    }
+    return int(lines);
+}
+
+int setLength(int length)
+{
+    if (length <= 0)
+    {
+        return 0;
+    }
+    return int(length);
+}
+
+int main(int argc, char **argv) {
+    int lines = 10;
+    int length = 10;
+
+
+    for (int i; i < argc; i++)  {
+        if (strcmp(argv[i], "-n") == 0 | strcmp(argv[i], "-lines") == 0)    {
+            cout << "Test1" << endl;
+        }
+
+        if (strcmp(argv[i], "-l") == 0 | strcmp(argv[i], "-length") == 0)
+        {
+            cout << "Test2" << endl;
+        }
+
+        if (strcmp(argv[i], "-d") == 0 | strcmp(argv[i], "-dict") == 0)
+        {
+            cout << "Test3" << endl;
+        }
+
+        if (strcmp(argv[i], "-o") == 0 | strcmp(argv[i], "-output") == 0)
+        {
+            cout << "Test4" << endl;
+        }
+        if (strcmp(argv[i], "-h") == 0 | strcmp(argv[i], "-help") == 0)
+        {
+            cout << "COMP 439 HW 1: Ceasar Cipher Encoder" << endl;
+            cout << " -n | -lines       Number of lines to generate (default = 10)" << endl;
+            cout << " -l | -length      Minimum word length (default = 3)" << endl;
+            cout << " -d | -dict        Location of dictionary containing the word list (default = wordlist.txt)" << endl;
+            cout << " -o | -output      Output filename. No input defaults to $STDOUT" << endl;
+            cout << " -h | -help        Display this message" << endl;
+            return 1;
+        }
+    }
+
+    return 0;
+}
